da2f7d1653331fd56ef0a995a7135aaef1cb282a
Bjørn Harbo Dahle <bdahle@hey.com>
2021-06-23 08:41:06 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import myName from "./myName";
+
+function component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = myName("Cody");
+  return element;
+}
+
+document.body.appendChild(component());
